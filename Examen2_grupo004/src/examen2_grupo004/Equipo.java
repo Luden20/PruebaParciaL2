20ab65d58e1199bfab0279c1f0d500a399de5262
DanielCalvopina <148661526+DanielCalvopina@users.noreply.github.com>
2024-05-06 07:52:30 -0500

    EQUPOS
    LISTA DE EQUIPOS

--- Examen2_grupo004/src/examen2_grupo004/Equipo.java
+++ Examen2_grupo004/src/examen2_grupo004/Equipo.java
@@ -1,22 +1,27 @@
 package examen2_grupo004;
 import java.util.LinkedHashMap;
-public class Equipos {
+public class Equipo {
     private String NombreEquipo;
     private String Codigo;
-    private LinkedHashMap<String,Entrenador> EntrenadorPasados = new LinkedHashMap<String,Entrenador>();
+    private LinkedHashMap<String,Entrenador> EntrenadoresPasados = new LinkedHashMap<String,Entrenador>();
     private Entrenador COACH;
     private LinkedHashMap<String,Deportista> MiembrosPasados = new LinkedHashMap<String,Deportista>();
     private LinkedHashMap<String,Deportista> MiembrosActuales = new LinkedHashMap<String,Deportista>();
-    public Equipos(String Codigo,String NombreEquipo, Entrenador COACH){
+    public Equipo(String Codigo,String NombreEquipo, Entrenador COACH){
         this.Codigo = Codigo;
         this.NombreEquipo = NombreEquipo;
         this.COACH = COACH;        
     }
-    /*
     public void IngresarMiembro(Deportista Jugador){
-        MiembrosActuales.put(Jugador.getCedula, Jugador);
+        MiembrosActuales.put(Jugador.getCedula(), Jugador);
     }
     public void RetirarMiembro(Deportista Jugador){
         MiembrosActuales.remove(Jugador.getCedula());
-    }*/
+        MiembrosPasados.put(Jugador.getCedula(),Jugador);
+    }
+    public void CambiarEntrenador(Entrenador COACH){
+        EntrenadoresPasados.put(COACH.getCedula(),COACH);
+        this.COACH = COACH;
+    }
+    public void 
 }
